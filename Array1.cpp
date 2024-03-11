2f76b5788baa3ac68ddea581391eac2d0aefa15c
samiul423 <mdsamiul4123@gmail.com>
2024-03-11 10:05:20 +0600

    array

---
+++ Array1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    int mark[5]={55,85,45,35,25};
+
+    for(int i=0;i<5; i++)
+    {
+        cout<<mark[i]<<" ";
+    }
+     
+
+         
+         
+
+
+   
+   
+}
